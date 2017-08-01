72e35307a31ce2415ce02d99e02e523b395a95e5
mateogolf <rodriguezml11@mail.wlu.edu>
2017-08-01 15:25:42 -0400

    initial notes on OOP

---
+++ pythonOOP.py
@@ -0,0 +1,21 @@
+class User(object):
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.logged = True
+    def login(self):
+        self.logged = True
+        print self.name + " is logged in."
+        return self
+    def logout(self):
+        self.logged = False
+        print self.name + " is not logged in"
+        return self
+    def show(self):
+        print "My name is {}. You can email me at {}".format(self.name, self.email)
+        return self
+
+# michael = User()
+# anna = User()
+new_user = User("Anna","anna@anna.com")
+print new_user.email
