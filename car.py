72554a9801599da44938e4f3a5dbe2b5aa3d293e
mateogolf <rodriguezml11@mail.wlu.edu>
2017-08-01 16:38:36 -0400

    Car Assignment

---
+++ car.py
@@ -0,0 +1,41 @@
+class Car(object):
+    def __init__(self, price, speed, fuel, mileage):
+        self.price = price
+        self.speed = speed
+        self.fuel = fuel
+        self.mileage = mileage
+        self.taxes()
+    def display_all(self):#display all
+        # print self.__dict__
+        # for attr, value in self.__dict__.iteritems():
+        #     print "{}: {}".format(attr,value)
+        print "Price: {}".format(self.price)
+        print "Speed: {}".format(self.speed)
+        print "Fuel: {}".format(self.fuel)
+        print "Mileage: {}".format(self.mileage)
+        print "Tax: {}".format(self.tax)
+        return self
+    def taxes(self):
+        if self.price > 10000:
+            self.tax=0.15
+        else:
+            self.tax=0.12
+        return self
+
+car1 = Car(2000, "35mph", "Full", "15mpg")
+car1.display_all()
+
+car2 = Car(2000,"5mph", "Not Full","105mpg")
+car2.display_all()
+
+car3 = Car(2000,"15mph", "Kind of Full","95mpg")
+car3.display_all()
+
+car4 = Car(2000,"25mph", "Full","25mpg")
+car4.display_all()
+
+car5 = Car(2000,"45mph", "Empty","25mpg")
+car5.display_all()
+
+car6 = Car(20000000,"35mph", "Empty","15mpg")
+car6.display_all()
